776917f385fb28ca7dfed95c70bac041648f6036
Subash Penneru <subashpenneru@gmail.com>
2020-10-20 15:15:43 +0530

    serving frontend as static

--- backend/server.js
+++ backend/server.js
@@ -35,6 +35,14 @@ app.get('/api/config/paypal', (req, res, next) =>
 const __dirname = path.resolve();
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  );
+}
+
 app.use(notFound);
 
 app.use(errorHandler);
